d8dbeb819ef840b56a7cd15bc4541bcb62276c44
Eduardo Kern Goes da Silva <edugoes215@gmail.com>
2026-07-20 21:16:32 -0300

    Atividade 4/5, meu portfólio

--- module-2/exercise-lists/components/header.js
+++ module-2/exercise-lists/components/header.js
@@ -1,7 +1,7 @@
 class Header extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
-            <header class="d-flex justify-content-between align-items-center py-4">
+            <header class="container d-flex justify-content-between align-items-center py-4">
                 <h1 id="logo">
                     Eduardo Goes
                 </h1>
@@ -10,18 +10,15 @@ class Header extends HTMLElement {
                         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                             <span class="navbar-toggler-icon"></span>
                         </button>
-                        <ul id="navbarNav" class="navbar-nav collapse navbar-collapse gap-5">
+                        <ul id="navbarNav" class="navbar-nav collapse navbar-collapse gap-3">
                             <li clas="nav-item">    
-                                <a class="nav-link" href="">Home</a>
+                                <a class="nav-link" href="/module-2/exercise-lists/index.html">Home</a>
                             </li>
                             <li clas="nav-item">    
-                                <a class="nav-link" href="">Sobre</a>
+                                <a class="nav-link" href="/module-2/exercise-lists/pages/about-me.html">Sobre</a>
                             </li>
                             <li clas="nav-item">
-                                <a class="nav-link" href="">Contato</a>
-                            </li>
-                            <li clas="nav-item">
-                                <a class="nav-link" href="">Projetos</a>
+                                <a class="nav-link" href="/module-2/exercise-lists/pages/contact.html">Contato</a>
                             </li>
                         </ul>
                     </div>
