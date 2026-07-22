7ba3245cee594c4653638e61b552ab99d54a0013
Eduardo Kern Goes da Silva <edugoes215@gmail.com>
2026-07-22 20:29:15 -0300

    Aula 03 e 04

---
+++ module-3/clarim-diario/src/App.jsx
@@ -0,0 +1,39 @@
+import Header from "./components/Header/Header"
+import NewsCard from "./components/NewsCard/NewsCard"
+import { noticias } from './data/noticias'
+import './App.css'
+import './styles/variables.css'
+import './styles/global.css'
+
+function App() {
+  const [ manchete, ...demais ] = noticias
+
+  return (
+    <>
+      <Header />
+
+      <main className="container">
+        <section className="manchete">
+          <NewsCard
+            categoria = {manchete.categoria}
+            titulo = {manchete.titulo}
+            resumo = {manchete.resumo}
+          />
+        </section>
+
+        <section className="grade">
+          {demais.map((noticia) => (
+            <NewsCard 
+              key = {noticia.id}
+              categoria = {noticia.categoria}
+              titulo = {noticia.titulo}
+              resumo = {noticia.resumo}
+            />
+          ))}
+        </section>
+      </main>
+    </>
+  )
+}
+
+export default App
